e0d3bf8e45dfa090cf9921739e31467d99baae5a
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2023-06-17 16:38:36 +0200

    Update RenameMeServiceProvider.php

--- src/Providers/RenameMeServiceProvider.php
+++ src/Providers/RenameMeServiceProvider.php
@@ -4,28 +4,14 @@ namespace Cerbero\:package_ns\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-/**
- * The service provider.
- *
- */
 class :package_nsServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Execute logic after the service provider is booted.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         //
     }
